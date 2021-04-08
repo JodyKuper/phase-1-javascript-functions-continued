ffb3582b5a7a5c1e5ab33e318ad02faada420293
Joel Cooper <jodycooper16@gmail.com>
2021-04-08 10:01:31 -0700

    commit tree 7040441addde7e0ddcf01b42622d633e4d0821fe

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function saturdayFun (activety="roller-skate"){
+  return `This Saturday, I want to ${activety}!`
+}
+ 
+const mondayWork= function(job="go to the office"){
+  return `This Monday, I will ${job}.`   
+  
+}
+
+function wrapAdjective(HL="*"){
+    return function(com="a hard worker") {
+        return `You are ${HL}${com}${HL}!`
+
+     
+
+}
+}    
+
+    
+     
+
+
